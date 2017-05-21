bc2b7531b86fe49cb149b7ac42262e02d1619a67
Zakir Durumeric <zakird@gmail.com>
2017-05-21 15:29:47 -0700

    enum factory (#15)
    * enum factory
    
    * accidental print

--- python/zsearch_definitions/anonstore.py
+++ python/zsearch_definitions/anonstore.py
@@ -1,14 +1,14 @@
 import sys
 import zsearch_definitions.certificate_pb2
+import zsearch_definitions.zlint_pb2
 
-class CertificateSource(object):
 
-    _by_pretty_name = {}
-    _by_name = {}
-    _by_value = {}
+class ZProtobufEnum(object):
 
     def __init__(self, value, name, pretty_name):
-        self.value, self.name, self.pretty_name = value, name, pretty_name
+        self.value = value
+        self.name = name
+        self.pretty_name = pretty_name
 
     @classmethod
     def from_value(cls, value):
@@ -29,93 +29,68 @@ class CertificateSource(object):
         return self.value.__cmp__(other.value)
 
 
-for _spid in zsearch_definitions.certificate_pb2.CertificateSource.values():
-    enum_name = zsearch_definitions.certificate_pb2.CertificateSource.Name(_spid)
-    simple_name = enum_name[len("CERTIFICATE_SOURCE_"):]
-    pretty_name = simple_name.lower()
-    object_ = CertificateSource(_spid, enum_name, pretty_name)
-    CertificateSource._by_name[enum_name] = object_
-    CertificateSource._by_value[_spid] = object_
-    CertificateSource._by_pretty_name[pretty_name] = object_
-    setattr(CertificateSource, simple_name, object_)
+def make_enum(name, definition, prefix):
+
+    class X(ZProtobufEnum):
+        _by_pretty_name = {}
+        _by_name = {}
+        _by_value = {}
+
+    X.__name__ = name
+    for _spid in definition.values():
+        enum_name = definition.Name(_spid)
+        simple_name = enum_name[len(prefix) + 1:]
+        pretty_name = simple_name.lower()
+        object_ = X(_spid, enum_name, pretty_name)
+        X._by_name[enum_name] = object_
+        X._by_value[_spid] = object_
+        X._by_pretty_name[pretty_name] = object_
+        setattr(X, simple_name, object_)
+
+    return X
 
 
-class CTPushStatus(object):
+CertificateSource = make_enum(
+        "CertificateSource",
+        zsearch_definitions.certificate_pb2.CertificateSource,
+        "CERTIFICATE_SOURCE")
 
-    _by_pretty_name = {}
-    _by_name = {}
-    _by_value = {}
+CTPushStatus = make_enum(
+        "CTPushStatus",
+        zsearch_definitions.ct_pb2.CTPushStatus,
+        "CT_PUSH_STATUS")
 
-    def __init__(self, value, name, pretty_name):
-        self.value, self.name, self.pretty_name = value, name, pretty_name
+CertificateParseStatus = make_enum(
+        "CertificateParseStatus",
+        zsearch_definitions.certificate_pb2.CertificateParseStatus,
+        "CERTIFICATE_PARSE_STATUS")
 
-    @classmethod
-    def from_value(cls, value):
-        return cls._by_value[value]
+CertificateType = make_enum(
+        "CertificateType",
+        zsearch_definitions.certificate_pb2.CertificateType,
+        "CERTIFICATE_TYPE")
 
-    @classmethod
-    def from_name(cls, name):
-        return cls._by_name[name]
+CertificateRevocationReason = make_enum(
+        "CertificateRevocationReason",
+        zsearch_definitions.certificate_pb2.CertificateRevocationReason,
+        "CERTIFICATE_REVOCATION_REASON")
 
-    @classmethod
-    def from_pretty_name(cls, name):
-        return cls._by_pretty_name[name]
+LintResultStatus = make_enum(
+        "LintResultStatus",
+        zsearch_definitions.zlint_pb2.LintResultStatus,
+        "LINT_RESULT")
 
-    def __hash__(self):
-        return self.value.__hash__()
+ZLintStatus = make_enum(
+        "ZLintStatus",
+        zsearch_definitions.zlint_pb2.ZLintStatus,
+        "ZLINT_STATUS")
 
-    def __cmp__(self, other):
-        return self.value.__cmp__(other.value)
+CTPushStatus = make_enum(
+        "CTPushStatus",
+        zsearch_definitions.ct_pb2.CTPushStatus,
+        "CT_PUSH_STATUS")
 
-
-for _spid in zsearch_definitions.ct_pb2.CTPushStatus.values():
-    enum_name = zsearch_definitions.ct_pb2.CTPushStatus.Name(_spid)
-    simple_name = enum_name[len("CT_PUSH_STATUS_"):]
-    pretty_name = simple_name.lower()
-    object_ = CTPushStatus(_spid, enum_name, pretty_name)
-    CTPushStatus._by_name[enum_name] = object_
-    CTPushStatus._by_value[_spid] = object_
-    CTPushStatus._by_pretty_name[pretty_name] = object_
-    setattr(CTPushStatus, simple_name, object_)
-
-
-
-class CertificateParseStatus(object):
-
-    _by_pretty_name = {}
-    _by_name = {}
-    _by_value = {}
-
-    def __init__(self, value, name, pretty_name):
-        self.value, self.name, self.pretty_name = value, name, pretty_name
-
-    @classmethod
-    def from_value(cls, value):
-        return cls._by_value[value]
-
-    @classmethod
-    def from_name(cls, name):
-        return cls._by_name[name]
-
-    @classmethod
-    def from_pretty_name(cls, name):
-        return cls._by_pretty_name[name]
-
-    def __hash__(self):
-        return self.value.__hash__()
-
-    def __cmp__(self, other):
-        return self.value.__cmp__(other.value)
-
-
-for _spid in zsearch_definitions.certificate_pb2.CertificateParseStatus.values():
-    enum_name = zsearch_definitions.certificate_pb2.CertificateParseStatus.Name(_spid)
-    simple_name = enum_name[len("CERTIFICATE_PARSE_STATUS_"):]
-    pretty_name = simple_name.lower()
-    object_ = CertificateSource(_spid, enum_name, pretty_name)
-    CertificateParseStatus._by_name[enum_name] = object_
-    CertificateParseStatus._by_value[_spid] = object_
-    CertificateParseStatus._by_pretty_name[pretty_name] = object_
-    setattr(CertificateParseStatus, simple_name, object_)
-
-
+CTServer = make_enum(
+        "CTServer",
+        zsearch_definitions.ct_pb2.CTServer,
+        "CT_SERVER")
