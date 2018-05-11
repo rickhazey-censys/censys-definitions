ce68bf89bef5e1e844b564669135bb6d806d164d
David Adrian <davidcadrian@gmail.com>
2018-05-11 15:15:14 -0400

    0.1.27: Relax dependencies on grpcio, protobuf

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "zsearch-definitions",
     description = "Egg version of files that can be used for interacting with zdb through python",
-    version = "0.1.25",
+    version = "0.1.27",
     license = "Apache License, Version 2.0",
     author = "Censys Team",
     author_email = "team@censys.io",
@@ -12,8 +12,8 @@ setup(
     keywords = "python json zsearch",
     zip_safe = False,
     install_requires = [
-        "grpcio==1.2.1",
-        "protobuf==3.2.0",
+        "grpcio>=1.2.1",
+        "protobuf>=3.2.0",
     ],
     packages = [
         "zsearch_definitions",
